16393b4d843de53d75138acd98e1b406bc2e19fb
Abraham Tiruneh <abraham.tiruneh7@gmail.com>
2026-05-24 01:06:23 +0300

    Remove unused template functions and keep only essential utilities

--- utility.h
+++ utility.h
@@ -5,31 +5,6 @@
 #include <string>
 using namespace std;
 
-// Template function: Swap two values of any type
-template <typename T>
-void swapData(T &a, T &b) {
-    T temp = a;
-    a = b;
-    b = temp;
-}
-
-// Template function: Display value with label
-template <typename T>
-void displayValue(const string &label, const T &value) {
-    cout << label << ": " << value << endl;
-}
-
-// Template function: Search for value in array
-template <typename T>
-int searchRecord(T arr[], int size, T key) {
-    for (int i = 0; i < size; i++) {
-        if (arr[i] == key) {
-            return i;
-        }
-    }
-    return -1;
-}
-
 // Utility function declarations
 void clearScreen();
 void pauseSystem();
